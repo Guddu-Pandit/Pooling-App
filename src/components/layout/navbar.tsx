53ba51e43f93009f59f04e257b9f7677635a278a
Guddu Kr. Pandit <guddukrp769@gmail.com>
2025-12-23 16:30:12 +0530

    making ui better for the logout button

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 
 export default async function Navbar() {
   const supabase = await createClient();
@@ -24,23 +25,23 @@ export default async function Navbar() {
   return (
     <nav className="border-b">
       <div className="mx-auto max-w-7xl px-6 py-4 flex items-center justify-between">
-        <Link href="/dashboard" className="font-bold text-xl">
+        <Link href="/dashboard" className="font-bold text-xl cursor-pointer">
           Polling App
         </Link>
 
-        <div className="flex items-center gap-4">
-          <Link href="/dashboard">Dashboard</Link>
+        <div className="flex items-center gap-4 cursor-pointer">
+          <Link href="/dashboard" className="text-black">Dashboard</Link>
 
           {/* ✅ ADMIN ONLY */}
           {role === "admin" && (
             <Link href="/admin">
-              <Button variant="secondary">Admin Panel</Button>
+              <Button variant="secondary" className="cursor-pointer bg-white text-black ">Admin Panel</Button>
             </Link>
           )}
 
           {user && (
             <form action="/auth/signout" method="post">
-              <Button variant="destructive">Logout</Button>
+              <Button variant="destructive" className="cursor-pointer text-red-500 bg-white hover:bg-red-200"><LogOut />Logout</Button>
             </form>
           )}
         </div>
